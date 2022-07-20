60a137764dd4d90f4e7315608841f6d5321792e4
Luc <luchio83@hotmail.fr>
2022-07-20 18:48:02 +0200

    reset main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React from 'react';
-import Home from './components/Home';
-import Navbar from './components/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
-        <Navbar />
+        <h1>REACT</h1>
+
       </header>
     </div>
   );
